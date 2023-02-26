09cb41fdf3e7e7b3847e0bf1c1a405f7a97043a4
yuefeng.niu <yuefeng.niu@17zuoye.com>
2023-02-27 00:06:43 +0800

    stop server gracefully

--- main.go
+++ main.go
@@ -1,25 +1,47 @@
 package main
 
 import (
+	"context"
 	"coredemo/framework"
 	"coredemo/framework/middleware"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	core := framework.NewCore()
-	// core.Use(
-	// 	middleware.Test1(),
-	// 	middleware.Test2())
+
 	core.Use(middleware.Recovery())
 	core.Use(middleware.Cost())
-	core.Use(middleware.Timeout(3 * time.Second))
 
 	registerRouter(core)
 	server := &http.Server{
 		Handler: core,
 		Addr:    ":8888",
 	}
-	server.ListenAndServe()
+
+	// 这个goroutine是启动服务的goroutine
+	go func() {
+		server.ListenAndServe()
+	}()
+
+	// 当前的goroutine等待信号量
+	quit := make(chan os.Signal)
+	// 监控信号：SIGINT, SIGTERM, SIGQUIT
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
+	// 这里会阻塞当前goroutine等待信号
+	<-quit
+
+	// 调用Server.Shutdown graceful结束
+	timeoutCtx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(timeoutCtx); err != nil {
+		log.Fatal("Server Shutdown:", err)
+	}
+	log.Println("优雅结束")
 }
